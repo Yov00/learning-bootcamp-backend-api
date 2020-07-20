d352573392dfdd89a1bab87ac089c20762c97de5
Yo! <yo@pop-os.localdomain>
2020-07-20 15:56:29 +0300

    Adding routes, controller, prepared CRUD methods

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-// Loead env variables
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
+// Load env variables
 dotenv.config({path:'./config/config.env'});
 
+// Mount routes
 const app = express();
+app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`Server running in:${process.env.NODE_ENV} on port: ${process.env.PORT}`))
+app.listen(PORT,console.log(`Server running in:${process.env.NODE_ENV} on port: ${process.env.PORT}`));
